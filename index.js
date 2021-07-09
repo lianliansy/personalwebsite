ac95c91d703f1569bc2081a3d8ef8575013a8174
lianliansy <lindseysiy@gmail.com>
2021-07-08 17:21:57 -0700

    added the showcase section

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const checkbox = document.querySelector('#check');
 const mainContent = document.querySelector('.mainContent');
 
-function checkMe() {
-    if(checkbox.checked == true) {
+checkbox.addEventListener('click', () => {
+    if(checkbox.checked) {
         mainContent.style.display = "none";
     } else {
         mainContent.style.display = "block";
     }
-}
+})
+
+
+const btnFlexbox = document.querySelector('#btn-flexbox');
+const btnGrid = document.querySelector('#btn-grid');
+const iframe = document.querySelector('#iframe');
+
+btnFlexbox.addEventListener('click', ()=>{
+    iframe.setAttribute("src", "https://codepen.io/LianLianC/embed/VwbaGNm?defaultTab=result");
+})
+
+btnGrid.addEventListener('click', () => {
+    iframe.setAttribute("src", "https://codepen.io/LianLianC/embed/KKmzwWB?defaultTab=result");
+})
+
+const cssBtn = document.querySelector('.css_btn');
+const list = document.querySelector('.css_list');
+
+cssBtn.addEventListener('click', () => {
+    list.classList.toggle('newlist');
+})
